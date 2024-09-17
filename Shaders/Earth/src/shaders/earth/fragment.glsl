5d328740ba45e0ef80357926272d62c545f21814
Zack Hagerty <70977089+ZackHagerty@users.noreply.github.com>
2024-09-16 23:25:19 -0400

    Setting up Earth shader

---
+++ Shaders/Earth/src/shaders/earth/fragment.glsl
@@ -0,0 +1,25 @@
+varying vec2 vUv;
+varying vec3 vNormal;
+varying vec3 vPosition;
+
+uniform sampler2D uDayTexture;
+uniform sampler2D uNightTexture;
+uniform sampler2D uSpecularCloudsTexture;
+
+void main()
+{
+    vec3 viewDirection = normalize(vPosition - cameraPosition);
+    vec3 normal = normalize(vNormal);
+    vec3 color = vec3(vUv, 1.0);
+
+    // Day / night color
+    vec3 dayColor = texture(uDayTexture, vUv).rgb;
+    vec3 nightColor = texture(uNightTexture, vUv).rgb;
+
+    color = nightColor;
+
+    // Final color
+    gl_FragColor = vec4(color, 1.0);
+    #include <tonemapping_fragment>
+    #include <colorspace_fragment>
+}
